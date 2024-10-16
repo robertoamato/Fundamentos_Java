f9ca3969c2d33507d8d940c08acf9051e9c3892e
robertoamato <robertoamatoz@gmail.com>
2024-10-16 19:09:50 -0300

    Comandos De Impressao

---
+++ ComandosDeImpressao.java
@@ -0,0 +1,9 @@
+public class ComandosDeImpressao {
+  public static void main(String[] args) {
+    System.out.println("Hello, World!"); // println = imprime na tela e pula linha
+    System.out.print("Hello, World!"); // print = imprime na tela sem pular linha
+    System.out.printf("Hello, World!"); // printf = imprime na tela e pula linha formatada pelo printf
+    System.out.format("Hello, World!"); // format = imprime na tela sem pular linha formatada pelo printf
+
+  }
+}
