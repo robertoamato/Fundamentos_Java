7272d6dffd774e9bf8df744d1ee6ba91729c4946
robertoamato <robertoamatoz@gmail.com>
2024-10-17 15:25:32 -0300

    Update ObjetoDaClasseScanner.java

--- ObjetoDaClasseScanner.java
+++ ObjetoDaClasseScanner.java
@@ -3,39 +3,20 @@ import java.util.Scanner;
 public class ObjetoDaClasseScanner {
     public static void main(String[] args) {
         // Criar um objeto Scanner para receber entrada do usuário
-        Scanner sc = new Scanner(System.in);
+        Scanner scanner1 = new Scanner(System.in);
 
-        // Declarar variáveis
-        int nota2 = 0;
-        int nota3 = 0;
-        String nome = "";
+        String nome = "" ;
+        System.out.println("Qual é o seu nome?");
+        nome = scanner1.nextLine();
+        System.out.println("Ola, " + nome + "!");
+        
+        scanner1.close();
 
-        // Receber o nome do usuário
-        System.out.println("Digite o seu nome: ");
-        nome = sc.nextLine();
-
-        // Receber a segunda nota
-        System.out.println("Digite a segunda nota: ");
-        nota2 = sc.nextInt();
-
-        // Receber a terceira nota
-        System.out.println("Digite a terceira nota: ");
-        nota3 = sc.nextInt();
-
-        // Calcular e exibir a média das notas
-        System.out.println(nome + ", a média é: " + (nota2 + nota3) / 2);
-
-        // Fechar o Scanner
-        sc.close();
    
-  Scanner scanner = new Scanner(System.in);
 
-  String nome1 = "";
-  System.out.println("Digite o seu nome: ");  
-  nome1 = scanner.nextLine();
-  System.out.println("O seu nome é: " + nome1); 
-
-  scanner.close();
 }
 
 }
+
+
+
