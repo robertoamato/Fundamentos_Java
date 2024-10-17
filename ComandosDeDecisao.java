8e1c07eaf356cf5834d2c0b1f0460a7830be03ec
robertoamato <robertoamatoz@gmail.com>
2024-10-17 12:37:14 -0300

    Create ComandosDeDecisao.java

---
+++ ComandosDeDecisao.java
@@ -0,0 +1,28 @@
+public class ComandosDeDecisao {  
+  public static void main(String[] args) {
+    int y = 20;
+    int x = 10;
+  
+    // Primeira verificação: se x é maior que y
+    if (x > y) {
+      System.out.println("x é maior que y");
+    } else {
+      System.out.println("x é menor que y");
+    }
+
+    // Variáveis para notas e faltas
+    int nota = 85;
+    int faltas = 10;
+    int maxFaltas = 5;
+    int media = 60;
+
+    // Segunda verificação: checar aprovação, recuperação ou reprovação
+    if (nota >= media && faltas < maxFaltas) {
+      System.out.println("Aprovado");
+    } else if (nota < media) {
+      System.out.println("Recuperação");
+    } else {
+      System.out.println("Reprovado");
+    }
+  }
+}
