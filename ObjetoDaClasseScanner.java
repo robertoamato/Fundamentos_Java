e88f53841c7f984048de094f157bb07f521e66d3
robertoamato <robertoamatoz@gmail.com>
2024-10-17 16:22:33 -0300

    Update ObjetoDaClasseScanner.java

--- ObjetoDaClasseScanner.java
+++ ObjetoDaClasseScanner.java
@@ -10,6 +10,23 @@ public class ObjetoDaClasseScanner {
         nome = scanner1.nextLine();
         System.out.println("Ola, " + nome + "!");
         
+      int idade = 0;
+        System.out.println("Qual é a sua idade?");
+        idade = scanner1.nextInt();
+        
+        
+      int nota = 0;
+        System.out.println("digite o valor da sua nota");
+        nota = scanner1.nextInt();  
+        System.out.println("A sua nota é: " + nota);
+
+        if (nota > 0) { 
+            System.out.println(" PARABENS, VOCE FOI APROVADO");
+        } else {
+            System.out.println("Reprovado");
+          
+        }
+
         scanner1.close();
 
    
