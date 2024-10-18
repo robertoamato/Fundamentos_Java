a1845dbe5581be259309f7b79537668e23af3d0b
robertoamato <robertoamatoz@gmail.com>
2024-10-18 18:36:20 -0300

    Conversão Implícita

---
+++ ConversaoImplicita.java
@@ -0,0 +1,15 @@
+package Curso;
+
+public class ConversaoImplicita {
+    public static void main(String[] args) {
+        //Conversao Explicita do meno para o maior
+
+        byte VarByte = 21;
+        int VarInt = VarByte;
+
+        System.out.println( VarInt );
+
+
+
+    }
+}
