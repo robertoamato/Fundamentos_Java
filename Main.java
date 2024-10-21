62ab0534ebef2556e5117a63875e19463c40a2f5
robertoamato <robertoamatoz@gmail.com>
2024-10-21 17:37:43 -0300

    Aula 13 Coleta de dados

--- Main.java
+++ Main.java
@@ -1,5 +1,22 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        // declaração de variaveis
+        Scanner teclado = new Scanner(System.in);
+        String Nome = "";
+        String Sobrenome ="";
+        int idade = 0;
+        // entrada de dados
+        System.out.print("Digite seu nome: ");
+        Nome = teclado.next();
+        System.out.print("Digite seu sobrenome: ");
+        Sobrenome = teclado.next();
+        System.out.print("Digite sua idade: ");
+        idade = teclado.nextInt();
+        // saida de dados
+        System.out.print("Boa tarde " + Nome + " " + Sobrenome + "!" + " Você tem " + idade + " anos.");
+
+
     }
 }
