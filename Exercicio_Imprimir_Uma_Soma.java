08d170dc8559ea0ef9530c2fa9649e21aa0e931d
robertoamato <robertoamatoz@gmail.com>
2024-10-21 18:49:51 -0300

    Exercicio Imprimir soma simples

---
+++ Exercicio_Imprimir_Uma_Soma.java
@@ -0,0 +1,11 @@
+public class Exercicio_Imprimir_Uma_Soma {
+
+    public static void main(String[] args) {
+        int numero1 = 6;
+        int numero2= 2;
+        int soma = 6 + 2;
+        System.out.println(" quanto é " + numero1 +  " + " + numero2 + "?");
+        System.out.println(" a soma dos valores é " + soma  );
+    }
+}
+        
