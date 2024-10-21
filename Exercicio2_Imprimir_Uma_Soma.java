8ef843586da6e281aadba659387a786cee07e930
robertoamato <robertoamatoz@gmail.com>
2024-10-21 19:51:05 -0300

    At

--- Exercicio2_Imprimir_Uma_Soma.java
+++ Exercicio2_Imprimir_Uma_Soma.java
@@ -1,11 +1,24 @@
+import java.util.Scanner;
+
+
 public class Exercicio2_Imprimir_Uma_Soma {
     public static void main(String[] args) {
 
-        int num1 = 78;
-        int num2 = 10;
-        int res = num2 - num1;
+       Scanner sc = new Scanner(System.in);
+        int num1 = 0;
+        int num2 = 0;
 
-        System.out.println(" 78 - 10  o resultado é  " +  res );
 
+        System.out.println("digite um numero"  );
+        num1 = sc.nextInt();
+
+        System.out.println("Digite um segundo numero" );
+        num2 = sc.nextInt();
+
+        int res = num1 + num2;
+
+        System.out.println("O valor da soma é "  +  res);
+
+        sc.close();
     }
 }
