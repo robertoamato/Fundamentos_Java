8c6d04a6aa875a510c9dda24fb6812b9e90e63ac
robertoamato <robertoamatoz@gmail.com>
2024-10-22 17:09:35 -0300

    Aula 16 Operadores Aritmeticos

---
+++ Operadores_Aritmeticos.java
@@ -0,0 +1,31 @@
+public class Operadores_Aritmeticos {
+
+    public static void main(String[] args) {
+
+     int numero1 = 6;
+     int numero2 = 2;
+
+     int soma = numero1 + numero2;
+     int subtracao = numero1 - numero2;
+     int multiplicacao = numero1 * numero2;
+     int divisao = numero1 / numero2;
+     int resto = numero1 % numero2;
+     System.out.println("Soma: " + soma);
+     System.out.println("Subtracao: " + subtracao);
+     System.out.println("Multiplicacao: " + multiplicacao);
+     System.out.println("Divisao: " + divisao);
+
+     int num =78;
+     int num1 = 10;
+     int soma1 = num + num1;
+     System.out.println("Soma: " + soma1);
+     int sub= num - num1;
+     System.out.println("Subtracao: " + sub);
+     int mult = num * num1;
+     System.out.println("Multiplicacao: " + mult);
+     int div = num / num1;
+     System.out.println("Divisao: " + div);
+     int res = num % num1;
+     System.out.println("Resto: " + res);
+}
+}
