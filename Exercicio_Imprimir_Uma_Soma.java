ecef202d4730a2cfdfb669d15ed612acf5e2df62
robertoamato <robertoamatoz@gmail.com>
2024-10-22 15:34:46 -0300

    Exercicios da aula 13 update

--- Exercicio_Imprimir_Uma_Soma.java
+++ Exercicio_Imprimir_Uma_Soma.java
@@ -1,10 +1,25 @@
+import java.util.Scanner;
+
 public class Exercicio_Imprimir_Uma_Soma {
 
     public static void main(String[] args) {
-        int numero1 = 6;
-        int numero2= 2;
-        int soma = 6 + 2;
-        System.out.println(" quanto é " + numero1 +  " + " + numero2 + "?");
-        System.out.println(" a soma dos valores é " + soma  );
+        //Declaraçao
+        int numero1 = 0;
+        int numero2= 0;
+        Scanner teclado = new Scanner(System.in);
+
+        //Entrada de dados
+
+        System.out.println("Digite um numero");
+        numero1= teclado.nextInt();
+
+        System.out.println("Digite um novo numero");
+        numero2 = teclado.nextInt();
+
+        int soma= numero1 + numero2;
+
+            // saida de dados
+        System.out.println(" o valor da soma é  "  + soma);
+
     }
 }
