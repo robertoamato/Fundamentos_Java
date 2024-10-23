44849902ee5159f13eed2a3450ec9637a09ac437
robertoamato <robertoamatoz@gmail.com>
2024-10-23 16:24:29 -0300

    Aula021_OperadoresRelacionais ATUALIZACAO

---
+++ Aula021_OperadoresRelacionais/src/Exericio_Operadores_Relacionais.java
@@ -0,0 +1,32 @@
+public class Exericio_Operadores_Relacionais {
+    public static void main(String[] args) {
+
+        // Declaração De Variaveis
+
+        int num1 = 5;
+        int num2 = 7;
+        Boolean res;
+
+        //maior >
+        res=  num1 > num2;
+        System.out.println(" 5 é maior que 7 ?  " +  res);
+
+        //maior ou igual >=
+        res = num1 >= num2;
+        System.out.println(" 5 é maior ou igual que 7 ?  " +  res);
+
+        //menor <
+        res = num1 < num2;
+        System.out.println(" 5 é menor que 7 ?  " +  res);
+
+        //menor ou igual <=
+         res = num1 <= num2;
+        System.out.println(" 5 é menor igual que 7 ?  " +  res);
+
+
+        //igual ==
+            res = num1==num2;
+        System.out.println(" 5 é igual que 7 ?  " +  res);
+
+
+}}
