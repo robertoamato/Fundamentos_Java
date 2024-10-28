0fc8b2c100bd4f2a4823aa9285a34733185d25f9
robertoamato <robertoamatoz@gmail.com>
2024-10-28 14:10:38 -0300

    Aula032_LacoFor

---
+++ Aula032_LacoFor/src/Ex016.java
@@ -0,0 +1,16 @@
+public class Ex016 {
+
+    public static void main(String[] args) {
+
+        int soma = 0;
+
+        for(int i = 1; i <= 100; i++ ) {
+            soma = soma + i;
+            //	System.out.println("Soma = " + soma);
+        }
+
+        System.out.println("Soma = " + soma);
+    }
+
+    }
+
---
+++ Aula032_LacoFor/src/ExemploDeLacoFor.java
@@ -0,0 +1,17 @@
+public class ExemploDeLacoFor {
+
+    public static void main(String[] args) {
+        //laço for
+        //variável e inicialização = contador
+        //incremento
+        //critério de parada
+
+        //sintaxe
+        int i;
+        for(i = 0 ;  i <= 10; i++) {
+
+            System.out.println(i);
+
+        }
+    }
+}
