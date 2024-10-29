0522015001647eb34396869c5825b2b879ebfb06
robertoamato <robertoamatoz@gmail.com>
2024-10-29 15:46:10 -0300

    Aula034_LacoWhile

---
+++ Aula034_LacoWhile/src/LacoWhile.java
@@ -0,0 +1,18 @@
+public class LacoWhile {
+
+    public static void main(String[] args) {
+
+        //laço while - loop
+        //sintaxe
+        //inicialização
+        int i = 1;
+        //critério de parada
+        while(i <= 5) {
+            System.out.println(i);
+            //incremento
+            i++; // i = i + 1
+        }
+
+        System.out.println("FIM");
+    }
+}
