9d786ce4d162a3cf2c9022e08da13ca53ebafb83
robertoamato <robertoamatoz@gmail.com>
2024-10-29 15:42:51 -0300

    update

---
+++ Aula032_LacoFor/src/ExemploDeLacoWhile.java
@@ -0,0 +1,17 @@
+public class ExemploDeLacoWhile {
+
+    public static void main(String[] args) {
+
+        //laço while - loop
+        //sintaxe
+        //inicialização
+        int i = 1;
+        //critério de parada
+         while (i <= 5) {
+         System.out.println(i);
+         //incremento
+         i++; // i = i + 1
+            }
+        }
+    }
+
---
+++ Aula032_LacoFor/src/Exercicio.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Exercicio {
+
+    public static void main(String[] args) {
+
+        Scanner  teclado = new Scanner(System.in);
+
+        for (int num1=0; 0<=15; num1 +=5)
+        System.out.println( teclado);
+
+
+
+    }
+}
