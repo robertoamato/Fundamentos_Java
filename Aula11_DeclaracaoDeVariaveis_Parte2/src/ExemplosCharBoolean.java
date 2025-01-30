ca2aebff22215bd2f65d92239e62c7dfa0885410
robertoamato <robertoamatoz@gmail.com>
2025-01-30 18:35:25 -0300

    Aula12 Variaveis e tipos de dados

---
+++ Aula11_DeclaracaoDeVariaveis_Parte2/src/ExemplosCharBoolean.java
@@ -0,0 +1,7 @@
+public class ExemplosCharBoolean {
+    public static void main(String[] args) {
+        char letra = 'a';
+        boolean verdadeiro = true;
+        boolean falso = false;
+    }
+}
---
+++ Aula11_DeclaracaoDeVariaveis_Parte2/src/ExemplosNumerosReais.java
@@ -0,0 +1,8 @@
+public class ExemplosNumerosReais {
+    public static void main(String[] args) {
+        double salario = 1250.70;
+        float altura = 1.70f;
+        System.out.println(salario);
+        System.out.println(altura);
+    }
+}
